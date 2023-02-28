6ca87697fa0f0ae8bb32e9d5648c18132fe2fed2
Gabriel <101840148+gaturama@users.noreply.github.com>
2023-02-28 10:00:54 -0300

    another commit

--- controller/Cidade.cs
+++ controller/Cidade.cs
@@ -10,6 +10,48 @@ namespace Controller
             }catch(Exception){
                 throw new Exception("Não foi possível cadastrar o cidade.");
             }
+            Model.Cidade cidade = new Model.Cidade(idConvert, nome);
         }
-    }
+        
+        public static void AlterarCidade(
+            string idCidade,
+            string nome
+        ){
+            int idConvert = 0;
+            try{
+                idConvert = Parse(idCidade)
+            }catch(Exception){
+                throw new Exception ("Este ID é inválido!");
+            }
+            Model.Cidade.AlterarCidade(idConvert, nome);
+        }
+        
+        public static void ExcluirCidade(string id)
+        {
+            int idConvert = 0;
+            try{
+                idConvert = int.Parse(idCidade);
+            }catch(Exception) {
+                throw new Exception ("Este ID é inválido!");
+            }
+            Model.Cidade.ExcluirCidade(idConvert);
+        }
+        
+           public static Model.Cidade BuscarCidade(string id)
+           {
+               int idConvert = 0;
+               try{
+                   idConvert = int.Parse(idCidade);
+               }catch(Exception) {
+                   throw new Exception ("Este ID é inválido!");
+               }
+
+               return Model.Cidade.BuscarCidade(idConvert);
+           }
+
+            public static List<Model.Cidade> ListarCidades()
+            {
+                return Model.Cidade.Cidades;
+            }
+        }
 }
