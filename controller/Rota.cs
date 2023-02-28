ed63f976afa0220c7ea23aa31ed3644f187b1cb9
Gabriel <101840148+gaturama@users.noreply.github.com>
2023-02-28 10:17:55 -0300

    another commit

--- controller/Rota.cs
+++ controller/Rota.cs
@@ -2,13 +2,89 @@ namespace Controller
 {
     public class Rota
     {
-        public static void CadastrarRota(int id, string origemId, string destinoId, string caminhaoId, string data){
+        public static void CadastrarRota(
+            int id, 
+            string origemId, 
+            string destinoId, 
+            string caminhaoId, 
+            string data
+        ){
             int idConvert  = 0;
             try{
                 idConvert = int.Parse(id);
-                }catch(Exception){
-                    throw new Exception("Não foi possivel cadastrar a rota.");
-                }
+            }catch(Exception){
+              throw new Exception("Este ID é inválido!");
+            }
+            int origemIdConvert = 0;
+            try{
+                origemIdConvert = int.Parse(origemId);
+            }catch(Exception) {
+                throw new Exception("Este ID é inválido!");
+            }    
+            
+            Model.Cidade origem = Model.Cidade.BuscarCidade(origemIdConvert);
+            Model.Cidade destino = Model.Cidade.BuscarCidade(int.Parse(destinoId));
+            Model.Caminhao caminhao = Model.Caminhao.BuscarCaminhao(int.Parse(idCaminhao));
+            DateTime dataConvert = DateTime.Parse(data);
+            Model.Rota rota = new Model.Rota(idConvert, origem, destino, caminhao, dataConvert);
+        }
+        
+        public static void AlterarRota(
+            string id,
+            string origemId,
+            string destinoId,
+            string idCaminhao
+            string data
+        ){
+            int idConvert  = 0;
+            try{
+                idConvert = int.Parse(id);
+            }catch(Exception){
+              throw new Exception("Este ID é inválido!");
+            }
+            
+            Model.Cidade origem = Model.Cidade.BuscarCidade(origemIdConvert);
+            Model.Cidade destino = Model.Cidade.BuscarCidade(int.Parse(destinoId));
+            Model.Caminhao caminhao = Model.Caminhao.BuscarCaminhao(int.Parse(idCaminhao));
+            DateTime dataConvert = DateTime.Parse(data);
+            Model.Rota.AlterarRota(idConvert, origem, destino, caminhao, dataConvert);
+        }
+        
+        public static void ExcluirRota(string id)
+        {
+            int idConvert  = 0;
+            try{
+                idConvert = int.Parse(id);
+            }catch(Exception){
+              throw new Exception("Este ID é inválido!");
+            }
+            Model.Rota.ExcluirRota(idConvert);
+        }
+        
+        public static Model.Rota BuscarRota(string id)
+        {
+            int idConvert = 0;
+            try {
+                idConvert = int.Parse(id);
+            } catch (Exception) {
+               throw new Exception("Id inválido");
+            }
+            return Model.Rota.BuscarRota(idConvert);
+        }
+        
+        public static List<string> ListarRotas()
+        {
+            List<string> stringRotas = new List<string>();
+            IEnumerable<Model.Rota> rotas = from rota in Model.Rota.Rotas
+                join origem in Model.Cidade.Cidades on rota.Origem.Id equals origem.Id
+                join destino in Model.Cidade.Cidades on rota.Destino.Id equals destino.Id
+                join caminhao in Model.Caminhao.Caminhoes on rota.Caminhao.Id equals idCaminhao
+                select rota;
+            
+            foreach (Model.Rota rota in rotas){
+                stringRotas.Add($"Id: {rota.Id}, Origem: {rota.Origem.Nome}, Destino: {rota.Destino.Nome}, Caminhão: {rota.Caminhao.Placa}, Data: {rota.Data}");
+            }
+            return stringRotas;
         }
     }
 }
