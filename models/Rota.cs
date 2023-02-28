43904a9170c70daded32bd815d88abc3d3c0c2d6
Gabriel <101840148+gaturama@users.noreply.github.com>
2023-02-28 09:52:59 -0300

    another commit

--- models/Rota.cs
+++ models/Rota.cs
@@ -2,8 +2,71 @@ using System;
 using System.Collections.Generic;
 
 namespace Model{
-
-  public int Id { get; set; }
-  public Cidade origem { get; set; }
   
+  public class Rota
+  {
+      public int Id { get; set; }
+      private int_origemId;
+      public Cidade origem { get; set; }
+      private int_destinoId;
+      public Cidade Destino { get; set; }
+      private int_caminhaoId;
+      public Caminhao Caminhao { get; set; }
+      public DateTime Data { get; set; }
+
+      public static List<Rota> Rotas { get; set; } = new List<Rota>();
+
+      public Rota(
+        int id,
+        Cidade origem,
+        Cidade destino,
+        Caminhao caminhao,
+        DateTime data
+      ){
+        Id = id;
+        Origem = origem;
+        _origemId = origem.Id;
+        Destino = destino;
+        _destinoId = destino.Id;
+        Caminhao = caminhao
+        _caminhaoId = caminhao.Id;
+        Data = data;
+        
+        Rotas.Add(this);
+      }
+      
+      public override string ToString()
+      {
+        return $"Id: {Id}, Origem: {Origem}, Destino: {Destino}, Caminhão: {Caminhao}, Data: {Data}";
+      }
+      
+      public static void AlterarRota(
+        int id,
+        Cidade origem,
+        Cidade destino,
+        Caminhao caminhao,
+        DateTime data
+      ){
+        Rota rota = BuscarRota(id);
+        rota.Origem = origem;
+        rota.Destino = destino;
+        rota.Caminhao = caminhao;
+        rota.Data = data;
+      }
+    
+      public static void ExcluirRota(int id)
+      {
+        Rota rota = BuscarRota(id);
+        Rotas.Remove(rota);
+      }
+    
+    public static Rota BuscarRota(int id)
+    {
+      Rota? rota = Rotas.Find(r => r.Id == id);
+            if (rota == null) {
+                throw new Exception("Rota não encontrada");
+            }
+          return rota;
+       } 
+    }
 }
