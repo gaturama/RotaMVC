a4e482c87e61ee695c47d569570ccc447c46aafb
Gabriel <101840148+gaturama@users.noreply.github.com>
2023-02-28 09:11:45 -0300

    another commit

---
+++ view/Cidade.cs
@@ -0,0 +1,52 @@
+namespace View
+{
+  public Class Cidade
+  {
+    public static void CadastrarCidade()
+    {
+      Console.WriteLine("Digite o ID da cidade: ");
+      string idCidade = Console.ReadLine();
+      Console.WriteLine("Digite o nome da cidade: ");
+      string nome = Console.ReadLine();
+      try{
+          Controller.Cidade.CadastrarCidade(idCidade, nome);
+          Console.WriteLine("Cidade cadastrada com sucesso!");
+      }catch(Exception e) {
+        Console.WriteLine($"Erro ao cadastrar cidade: {e.Message}");
+      }
+   }
+   
+   public static void AlterarCidade()
+   {
+      Console.WriteLine("Digite o ID da cidade: ");
+      string idCidade = Console.ReadLine();
+      Console.WriteLine("Digite o nome da cidade: ");
+      string nome = Console.ReadLine();
+      try{
+          Controller.Cidade.AlterarCidade(idCidade, nome);
+          Console.WriteLine("Cidade cadastrada com sucesso!");
+      }catch(Exception e) {
+        Console.WriteLine($"Erro ao cadastrar cidade: {e.Message}");
+      }
+   }
+   
+   public static void ExcluirCidade()
+   {
+      Console.WriteLine("Digite o ID da cidade: ");
+      string idCidade = Console.ReadLine();
+      try{
+          Controller.Cidade.ExcluirCidade(idCidade);
+          Console.WriteLine("Cidade cadastrada com sucesso!");
+      }catch(Exception e) {
+        Console.WriteLine($"Erro ao cadastrar cidade: {e.Message}");
+      }
+   }
+   
+   public static void ListarCidades() {
+      Console.WriteLine("Listar cidades");
+      foreach (Model.Cidade cidade in Controller.Cidade.ListarCidades()) {
+        Console.WriteLine(cidade);
+      }
+   }
+}
+}
