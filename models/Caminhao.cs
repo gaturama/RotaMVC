9508b6c587a58ef6370a92f67673495f722a889d
Gabriel <101840148+gaturama@users.noreply.github.com>
2023-02-28 09:25:31 -0300

    another commit

--- models/Caminhao.cs
+++ models/Caminhao.cs
@@ -6,16 +6,46 @@ namespace Model
     public class Caminhao
     {
         public int idCaminhao { get; set; }
-        public string placa { get; set; }
-        public string motorista { get; set; }
+        public string Placa { get; set; }
+        public string Motorista { get; set; }
 
-        public static List<Caminhao> caminhaos = 
+        public static List<Caminhao> Caminhaos ={ get; set; } = new List<Caminhao>();
 
-        public Caminhao(int IdCaminhao, string Placa, string Motorista)
+        public Caminhao(int IdCaminhao, string placa, string motorista)
         {
             IdCaminhao = idCaminhao;
             Placa = placa;
             Motorista = motorista;
+            
+            Caminhaos.Add(this);
         }
-    }
+        
+        public override string ToString()
+        {
+            return $"Id: {idCaminhao}, Placa: {placa}, Motorista: {motorista}";
+        }
+        
+        public static void AlterarCaminhao (
+            int idCaminhao, string placa, string motorista)
+            {
+                Caminhao caminhao = BuscarCaminhao(idCaminhao);
+                caminhao.Placa = placa;
+                caminhao.Motorista = motorista;
+            }
+        
+        public static void ExcluirCaminhao (int idCaminhao)
+        {
+            Caminhao caminhao = BuscarCaminhao(idCaminhao);
+            Caminhaos.Remove(caminhao);
+        }
+        
+        public static void Caminhao BuscarCaminhao(int idCaminhao)
+            {
+                Caminhao? caminhao = Caminhaos.Find(c => c.Id ==idCaminhao);
+                if(caminhao == null) {
+                    throw new Exception("Esta cidade não foi encontrada!");
+                    }
+                return cidade;
+            }
+      }
 }
